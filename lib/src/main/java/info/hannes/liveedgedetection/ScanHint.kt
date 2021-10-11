d9acae9e2f64d80074a493340444f69b372d2a9c
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2021-10-11 20:45:28 +0800

    Remove-useless-files

--- lib/src/main/java/info/hannes/liveedgedetection/ScanHint.kt
+++
@@ -1,8 +0,0 @@
-package info.hannes.liveedgedetection
-
-/**
- * Enum that defines receipt detection messages
- */
-enum class ScanHint {
-    MOVE_AWAY, MOVE_CLOSER, FIND_RECT, ADJUST_ANGLE, CAPTURING_IMAGE, NO_MESSAGE
-}
